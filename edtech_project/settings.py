a7d8349bf29b82ca8d8e401de0233af2aa94dc03
DevenderNarra <devendernarra@gmail.com>
2025-06-26 21:58:28 +0530

    added the vm Ip address

--- edtech_project/settings.py
+++ edtech_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-r+11y&qvqesm-^3$905qwb^%8bfp1!f8fmplrsp^%75i3p*22f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['34.9.200.189', 'localhost', '127.0.0.1']
 
 
 # Application definition
